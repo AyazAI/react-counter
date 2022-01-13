cdf197225eb9eab94ffd69da0b754fa897df247a
AyazAI <ayaz.uiit3@gmail.com>
2022-01-13 09:13:09 +0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { useCountUp } from 'react-countup';
 
-export default function App() {
+const App = () => {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <CompleteHook />
     </div>
   );
-}
+};
+
+const CompleteHook = () => {
+  const countUpRef = React.useRef(null);
+  const { start, pauseResume, reset, update } = useCountUp({
+    ref: countUpRef,
+    start: 0,
+    end: 1234567,
+    delay: 1000,
+    duration: 5,
+    onReset: () => console.log('Resetted!'),
+    onUpdate: () => console.log('Updated!'),
+    onPauseResume: () => console.log('Paused or resumed!'),
+    onStart: ({ pauseResume }) => console.log(pauseResume),
+    onEnd: ({ pauseResume }) => console.log(pauseResume),
+  });
+  return (
+    <div>
+      <div ref={countUpRef} />
+      <button onClick={start}>Start</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={pauseResume}>Pause/Resume</button>
+      <button onClick={() => update(2000)}>Update to 2000</button>
+    </div>
+  );
+};
+
+export default App;
